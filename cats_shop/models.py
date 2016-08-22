2e868dbc670e2f6b9e49d96a2385484ebaeff25c
testpysoft <sg.sasha.dubovik@gmail.com>
2016-08-23 02:47:46 +0300

    Model Cats with Color Breed (enum sex)

--- cats_shop/models.py
+++ cats_shop/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django_enumfield import enum
+
 
 # Create your models here.
+
+
+class GenderEnum(enum.Enum):
+    MALE = 1
+    FEMALE = 2
+
+    labels = {
+        MALE: 'Male',
+        FEMALE: 'Female',
+    }
+
+
+class Breed(models.Model):
+    name = models.CharField(max_length=50)
+    desc = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
+
+class CatColor(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Cat(models.Model):
+    breed = models.ForeignKey(Breed)
+    sex = enum.EnumField(GenderEnum)
+    cat_color = models.ForeignKey(CatColor)
+    date = models.DateField()
+    desc = models.CharField(max_length=256)
+    photo = models.ImageField(upload_to='cats')
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.id
