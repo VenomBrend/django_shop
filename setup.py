85cde1b48e70d57c8036014f721be5e2570db453
testpysoft <sg.sasha.dubovik@gmail.com>
2016-08-23 15:01:30 +0300

    add dependenses for enum and ImageField in Model

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'Cats Shop',
-    version = '0.1',
-    packages = find_packages(),
-    install_requires = ['Django==1.8'],
-    description = 'Cats Shop on Django',
-    )
+    name='Cats Shop',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['Django==1.8',
+                      'six=1.10.0',
+                      'enum34=1.1.6',
+                      'django-enumfields==0.8.2',
+                      'Pillow==3.3.1'
+                      ],
+    description='Cats Shop on Django',
+)
