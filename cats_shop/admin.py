b98dfc0b1aa8cedda72590e1e9eed61ba865499d
hddn <hddnsmile@gmail.com>
2016-09-08 23:51:18 +0300

    add order model

--- cats_shop/admin.py
+++ cats_shop/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
-from .models import Breed, CatColor, Cat, Album
+from .models import Breed, CatColor, Cat, Album, Order, OrderPosition
 
-
-# Register your models here.
-# admin.site.register(CatColor)
 
 class BreedAdmin(admin.ModelAdmin):
     fields = ['name', 'desc']
@@ -13,6 +10,7 @@ class AlbumInline(admin.TabularInline):
     model = Album
     extra = 1
 
+
 class CatAdmin(admin.ModelAdmin):
     fields = ['breed', 'cat_color', 'sex', 'desc',
               'date', 'price']
@@ -20,7 +18,18 @@ class CatAdmin(admin.ModelAdmin):
     list_display = ('breed', 'cat_color', 'price', 'date')
 
 
+class OrderPositionInline(admin.TabularInline):
+	model = OrderPosition
+	extra = 1
+
+
+class OrderAdmin(admin.ModelAdmin):
+	inlines = [OrderPositionInline,]
+	readonly_fields = ['created', 'closed']
+	list_display = ('id', 'price', 'customer', 'created')
+
 
 admin.site.register(Breed, BreedAdmin)
 admin.site.register(CatColor)
 admin.site.register(Cat, CatAdmin)
+admin.site.register(Order, OrderAdmin)
