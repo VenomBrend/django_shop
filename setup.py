a7a787f8130bf22e1e9a6f328bae5365fe57591c
hddn <hddnSmile@gmail.com>
2016-09-12 18:39:15 +0300

    setup.py fix

--- setup.py
+++ setup.py
@@ -5,15 +5,13 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=['Django==1.8',
-                      'six=1.10.0',
-                      'enum34=1.1.6',
+                      'six==1.10.0',
+                      'enum34==1.1.6',
                       'django-enumfields==0.8.2',
                       'Pillow==3.3.1',
                       'lxml==3.6.4',
                       'cssselect==0.9.2',
                       'pyquery==1.2.13',
-
-
                       ],
     description='Cats Shop on Django',
 )
