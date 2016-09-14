b31a2bd628320a64d1c69b5b2a6526c4a50d96da
Kostiantyn Promskyi <cosprm@gmail.com>
2016-09-14 16:02:29 +0300

    Added views/templates, removed uselles breed-redefinition from admin.py

--- cats_shop/views.py
+++ cats_shop/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView, DetailView
+from .models import Album
 
-# Create your views here.
+class CatsList(ListView):
+    model = Album
+    context_object_name = 'cats_list'
+    template_name = 'cats_shop/index.html'
+
+
+class CatDetail(DetailView):
+    model = Album
+    context_object_name = 'cat'
+    template_name = 'cats_shop/detail.html'
--- django_shop/urls.py
+++ django_shop/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 
-urlpatterns = [    
+urlpatterns = [
+    url(r'^shop/', include('cats_shop.urls', namespace="cats_shop")),
     url(r'^admin/', include(admin.site.urls)),
     ] + static(
         settings.STATIC_URL
