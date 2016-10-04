94430732caa46c8dafdb51202465f7f674315b36
hddn <hddnsmile@gmail.com>
2016-10-05 00:38:29 +0300

    Create edit profile views, forms and templates

--- django_shop/views.py
+++ django_shop/views.py
@@ -1,5 +1,10 @@
-from django.views.generic import DetailView, UpdateView
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views.generic import DetailView, FormView
 
+from .forms import UserEditForm, ProfileEditForm
+
+from django.contrib.auth.models import User
 from .models import UserProfile
 from cats_shop.models import Order
 
@@ -15,7 +20,39 @@ class UserProfileDetail(DetailView):
         return context
 
 
-class UserProfileUpdate(UpdateView):
-    model = UserProfile
-    fields = '__all__'
+class UserProfileUpdate(FormView):
     template_name = 'profile_edit.html'
+
+    def get(self, request, *args, **kwargs):
+        user = User.objects.get(pk=request.user.id)
+        user_edit_form = UserEditForm(instance=user)
+        profile_edit_form = ProfileEditForm(instance=user.profile)
+        context = super(UserProfileUpdate, self).get_context_data(
+            user_edit_form=user_edit_form,
+            profile_edit_form=profile_edit_form)
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        user = User.objects.get(pk=request.user.id)
+        user_edit_form = UserEditForm(request.POST, instance=user)
+        profile_edit_form = ProfileEditForm(request.POST)
+
+        if user_edit_form.is_valid():
+            created_user = user_edit_form.save(commit=False)
+            profile_edit_form = ProfileEditForm(request.POST,
+                                                instance=user.profile)
+            if profile_edit_form.is_valid():
+                created_user.save()
+                profile_edit_form.save()
+                return HttpResponseRedirect(
+                    u'{}?status_message={}'.format(
+                        reverse('profile', kwargs={'pk': request.user.id}),
+                        ('User profile updated!')))
+        else:
+            return self.form_invalid(self, user_edit_form,
+                                     profile_edit_form, **kwargs)
+
+    def form_invalid(self, user_edit_form, profile_edit_form, **kwargs):
+        return self.render_to_response(self.get_context_data(
+            user_edit_form=user_edit_form,
+            profile_edit_form=profile_edit_form))
