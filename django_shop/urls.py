4e5fca0f9f8235cd780fc74a975635afc5f6eb6c
hddn <hddnsmile@gmail.com>
2016-10-05 10:18:12 +0300

    Add login_required decorator to profile urls

--- django_shop/urls.py
+++ django_shop/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 
 from django_shop import views
 
@@ -11,9 +12,11 @@ urlpatterns = [
     url(r'^', include('cats_shop.urls', namespace='cats_shop',
                       app_name="cats_shop")),
     url(r'^cart/', include('cart.urls', namespace='cart', app_name='cart')),
-    url(r'^profile/(?P<pk>\d+)/$', views.UserProfileDetail.as_view(),
+    url(r'^profile/(?P<pk>\d+)/$',
+        login_required(views.UserProfileDetail.as_view()),
         name='profile'),
-    url(r'^profile/(?P<pk>\d+)/update/$', views.UserProfileUpdate.as_view(),
+    url(r'^profile/(?P<pk>\d+)/update/$',
+        login_required(views.UserProfileUpdate.as_view()),
         name='profile_edit')
 
     ] + static(
