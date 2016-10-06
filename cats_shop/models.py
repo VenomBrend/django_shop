e69cf30a2fc316a017374a36000c0d4cc6e2df78
testpysoft <sg.sasha.dubovik@gmail.com>
2016-10-06 16:54:36 +0300

    delete breed parser list

--- cats_shop/models.py
+++ cats_shop/models.py
@@ -2,10 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from enumfields import EnumIntegerField
 from enumfields import Enum  # Uses Ethan Furman's "enum34" backport
-import parser_breeds
 
 
-BREEDS = parser_breeds.get_breeds()
+
 
 
 class GenderEnum(Enum):
@@ -50,7 +49,6 @@ class Album(models.Model):
 
 
 class Order(models.Model):
-
     class Meta:
         verbose_name = 'Order'
         verbose_name_plural = 'Orders'
@@ -80,7 +78,6 @@ class Order(models.Model):
 
 
 class OrderPosition(models.Model):
-
     class Meta:
         verbose_name = 'Order position'
         verbose_name_plural = 'Order positions'
