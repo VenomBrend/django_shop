ab6f10bbefb8c315787498d5b1b91fd097b0086b
Kostiantyn Promskyi <cosprm@gmail.com>
2016-10-06 12:50:30 +0300

    added db filling

--- cats_shop/migrations/0002_auto_20160913_0719.py
+++ cats_shop/migrations/0002_auto_20160913_0719.py
@@ -1,36 +1,69 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 from django.db import models, migrations
-from urllib.request import urlopen
-#from urllib2 import urlopen
-from lxml.html import fromstring
-from pyquery import PyQuery as pq
 
-URL = 'https://en.wikipedia.org/wiki/List_of_cat_breeds'  # site's url for parser
-ITEM_PATH = 'table.wikitable tr '  # tags to start
+import lxml.html as html
+import requests
+from urllib.parse import urljoin
 
+from django_shop.settings import MEDIA_ROOT
 
-def parse_breed(url, item_path):
-    f = urlopen(url)
-    list_html = f.read().decode('utf-8')
-    list_doc = fromstring(list_html)
-    rows = list_doc.cssselect(item_path)[1:]  # 1 bf first tr miss
-    breeds = []
-    for row in rows:
-        cols = row.cssselect('td')  # select td
-        a = cols[0].cssselect('a')  # select a in td
-        breeds.append(pq(a).attr('title'))
-    return breeds
+import os
 
 
-def filling_breeds(apps, schema_editor):
-    breeds_list = parse_breed(URL, ITEM_PATH)
+response = requests.get('https://en.wikipedia.org/wiki/List_of_cat_breeds')
+
+# Parse the body into a tree
+parsed_body = html.fromstring(response.text)
+
+def breeds_parse(response, parsed_body):
+    # Perform xpaths on the tree
+    breeds_list = parsed_body.xpath('//table/tr/td/a/text()')
+    return breeds_list
+
+breeds_list = breeds_parse(response, parsed_body)
+
+os.makedirs(MEDIA_ROOT + '/cats', exist_ok=True)
+
+def images_parse(response, parsed_body, breeds_list):
+
+    #We are taking onle first 10 images
+    images_route_list = parsed_body.xpath("//table/tr/td[7]/a/@href")[0:10]
+
+    # Convert any relative urls to absolute urls
+    images_absolute_route_list = [urljoin("https://en.wikipedia.org", image_route) for image_route in images_route_list]
+
+    # For creating images names appropriate for breeds, didn't find best way ._.
+    breeds_list = breeds_list[::-1]
+
+    for url in images_absolute_route_list:
+        response = requests.get(url)
+        parsed_body = html.fromstring(response.text)
+
+        # Pulling alone element from list
+        full_image_url = parsed_body.xpath("//div[@class='fullImageLink']/a/@href")[0]
+
+        full_image_absolute_url = urljoin("https:", full_image_url)
+
+        r = requests.get(full_image_absolute_url)
+
+        with open(MEDIA_ROOT + '/cats/%s' % breeds_list.pop(), 'wb') as f:
+            f.write(r.content)
+
+images_parse(response, parsed_body, breeds_list)
+
+
+def breeds_filling(apps, schema_editor):
     Breed = apps.get_model('cats_shop', 'Breed')
-    db_alias = schema_editor.connection.alias    
+    db_alias = schema_editor.connection.alias
     for breed in breeds_list:
-        if breed != None:
-            Breed.objects.using(db_alias).create(name=breed, desc='')
-            
+        Breed.objects.using(db_alias).create(name=breed, desc='')
+
+
+def colors_filling(apps, schema_editor):
+    CatColor = apps.get_model('cats_shop', 'CatColor')
+    db_alias = schema_editor.connection.alias
+    colors = ['black', 'gray', 'white']
+    for color in colors:
+        CatColor.objects.using(db_alias).create(name=color)
 
 
 class Migration(migrations.Migration):
@@ -39,5 +72,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(filling_breeds),
-    ]
+        migrations.RunPython(breeds_filling),
+        migrations.RunPython(colors_filling),
+]
